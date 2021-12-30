98ce0a352c86c00346038210fc86c31c0ef173ac
manyukh90 <manyukh90@gmail.com>
2021-12-30 14:52:09 +0700

    fix workspace path for job execution

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-echo *** Building Maven ***
+echo *** Building Maven Jar***
 
-docker run --rm --name maven_contanier -v $PWD/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3.8.2-ibmjava-alpine "$@"
+WORKSPACE=/home/vicha-p/jenkins-data/jenkins_home/workspace/pipeline-maven-udemy
+
+docker run --rm --name maven_contanier -v $WORKSPACE/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3.8.2-ibmjava-alpine "$@"
--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -2,4 +2,6 @@
 
 echo *** Testing Maven ***
 
-docker run --rm --name maven_contanier -v $PWD/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3.8.2-ibmjava-alpine "$@"
+WORKSPACE=/home/vicha-p/jenkins-data/jenkins_home/workspace/pipeline-maven-udemy
+
+docker run --rm --name maven_contanier -v $WORKSPACE/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3.8.2-ibmjava-alpine "$@"
